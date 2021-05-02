cdecc1267d193e2c137aa174365bab64377d0471
Pablo M Menezes <pablo.menezes@banese.com.br>
2021-05-02 19:23:42 -0300

    Adicionado detalhes no módulo RG

---
+++ resources-groups/rg-outputs.tf
@@ -0,0 +1,9 @@
+output "rg_name" {
+  value = azurerm_resource_group.id
+}
+output "rg_location" {
+  value = azurerm_resource_group.location
+}
+ output "rg_tags" {
+   value = azurerm_resource_group.tags
+ }
